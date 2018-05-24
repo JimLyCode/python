085cd76a5bf8b9202c14d173a2bb32a55d9a9c36
JimLy <1072594307@qq.com>
2018-05-24 16:42:03 +0800

    python for basic grammar

--- python-normal/basicGrammar.py
+++ python-normal/basicGrammar.py
@@ -1,6 +1,7 @@
 import keyword
-print("Python key words: " + ','.join(keyword.kwlist));
-print("Hello, Python!");
+import sys
+print("Python key words: " + ','.join(keyword.kwlist))
+print("Hello, Python!")
 
 # 注释1
 '''
@@ -9,10 +10,23 @@ print("Hello, Python!");
 """
 注释3-多行注释
 """
-num = 3;
+
+num = 3
 if num == 1:
-    print("true");
+    print("true")
 elif num == 3:
-    print(12 + num);
+    print("num is: "); print(12 + num)  # 同一行中使用多条语句，语句之间使用分号(;)分割
 else:
-    print("false");
+    print("false");print(" False")
+
+paragraph = """this is a paragraph,
+this is a paragraph.
+"""
+print(paragraph)
+
+# input("按下 enter 键退出，其他任意键显示...\n")
+
+print("123 ", "456")
+
+# 打印脚本传入参数, sys.argv[0] 代表文件本身路径, 所带参数从 sys.argv[1] 开始
+print(sys.argv)

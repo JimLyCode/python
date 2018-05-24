6044695803cd10d7f9dc45db09f00528a9829ba6
JimLy <1072594307@qq.com>
2018-05-24 16:08:18 +0800

    Initial commit

---
+++ python-normal/basicGrammar.py
@@ -0,0 +1,18 @@
+import keyword
+print("Python key words: " + ','.join(keyword.kwlist));
+print("Hello, Python!");
+
+# 注释1
+'''
+注释2-多行注释
+'''
+"""
+注释3-多行注释
+"""
+num = 3;
+if num == 1:
+    print("true");
+elif num == 3:
+    print(12 + num);
+else:
+    print("false");
